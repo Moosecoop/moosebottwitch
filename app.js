386c6e8dcaad2a708ea949ea8beb953784296e4b
moosecoop <cooper@makhijani.us>
2016-12-31 20:18:42 -0800

    LOTS OF THINGS

--- app.js
+++ app.js
@@ -20,12 +20,270 @@ client.connect();
 
 client.on('connected', function(address, port) {
     console.log(`Address: ${address} - Port: ${port}`);
-    client.action("luke_lafr", "MooseBot_ will remind people how the giveaway works periodicly!...... Also @Flight1simltt is cool!");
+    //client.action("luke_lafr", "MooseBot_ will remind people how the giveaway works periodicly!......");
 });
 
-setInterval(function() {
-    client.action("luke_lafr", "Winners of the game giveaways must whisper @Flight1simltt to claim their prizes!");
-}, 60000);
+/*setInterval(function() {
+    client.action("luke_lafr", "WINNERS OF THE GAME GIVEAWAY PLEASE MESSAGE @Flight1simltt YOUR UNIQUE DISCORD USERNAME AND TAG! THANKS!");
+}, 120000);*/
+
+client.on("resub", function (channel, username, months, message) {
+    client.action("luke_lafr", "Listen to @Moobot , it's sub thanking message is better lol");
+    if (months = 36) {
+        client.action("luke_lafr", "Wow! " + username + "! 3 Years! Congrats!");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?HELP";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "?luke ?teak ?nitro ?game ?subscribe ?amo ?crayon ?wyre ?donate ?extralife ?emma ?barrington ?job ?about ?moose ?mape ?jake ?csgo ?caltane ?overwatch");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "QUESTION ANSWERED ";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "CONGRATS! WINNER PLEASE MESSAGE @Flight1simltt WITH YOUR UNIQUE DISCORD NAME AND TAG! THANK YOU!");
+    }
+});
+
+
+client.on("chat", function(channel, user, message, self) {
+    if(message === "!extralife") {
+        client.action("luke_lafr", "its for the kids! moosecoop.com studies show using this link is better");
+    }
+});
+
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "THAT KEYBOARD";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "DEAL WITH IT! KEYBOARD WARRIORS UNITE!");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?GAME";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "MOOSECOOP'S GAME: http://bit.ly/2iRx3YV");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?ABOUT";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "MooseBot_ is a Twitch Bot built for luke_lafr's twitch chat. Created by moosecoop in javascript");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?LUKE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "luke_lafr is pretty much the dopest dude on planet earth");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?BARRINGTON";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "LordBarrington is the best ban hammer spammer ever!");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?NITRO";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "nitroblastdigital.......... kappa");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?EMMA";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "EMMA IS COOL -AUTUMN CHILD");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?DONATE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "Donate to luke_lafr (not extralife): https://streamtip.com/t/luke_lafr");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?EXTRALIFE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "donate to extralife! hellbaskets.com");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?WYRE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "WyreTheWolf is a pretty cool guy, also he's probably a better dad than you.");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?CRAYON";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "He'll draw you a picture with his imagination, or his crayons");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?TEAK";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "He'll teak the settings till they're JUST right");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?MOOSE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "Check out moosecoop, my creator: moosecoop.com - youtube.com/user/moosecoop");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?MAPE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "is he a maple leaf? who knows?");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?JAKE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "jake pls... (credit typicaljake.com)");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?CSGO";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "CS:NO");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?OVERWATCH";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "no pls");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?CSGO";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "CS:NO");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?CALTANE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "SIPPY P! http://bit.ly/2ikOss9");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?JOB";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "Luke works at LMG linusmediagroup.com");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?SUBSCRIBE";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "Subscribe to Luke! http://bit.ly/2ikGNKf");
+    }
+});
+
+client.on("chat", function(channel, user, message, self) {
+    var prefix = "?AMO";
+    var message4p = message.toUpperCase();
+    var prefixUsed = message4p.startsWith(prefix); // false
+    console.log(prefixUsed + " " + message4p + " " + prefix);
+    if(prefixUsed) {
+        client.action("luke_lafr", "normaly i dont do non mod's but ok, don't ask tho..... pls");
+    }
+});
+
+
+//client.on("chat", function)
 
 
 
@@ -39,4 +297,4 @@ setInterval(function() {
        }
     }
 
-    });*/
+});*/
